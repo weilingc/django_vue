ec85282635b5cc45f1eafb9e73e4688c07c9b0c6
weilingc <madeintw66@gmail.com>
2023-03-19 19:30:23 +0800

    update

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'add_book$', views.add_book),
-    re_path(r'show_books$', views.show_books)
+    re_path(r'show_books$', views.show_books),
+    path('test_pug/', views.TestPugView.as_view()),
 ]
